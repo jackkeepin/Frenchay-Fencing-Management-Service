0a10b89d6341b97a9ef6bae9f9879b9066bbf9a2
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-03-01 21:54:43 +0000

    Initial (unfinished) model for Quote

--- quote/models.py
+++ quote/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    customer_first_name = models.CharField(max_length=50)
+    customer_last_name = models.CharField(max_length=50)
+    address = {}
+    customer_email = models.EmailField()
+    materials = {}
+    job_descripton = models.TextField()
+    price_of_materials = models.DecimalField(decimal_places=2)
+    removal_included = models.BooleanField(null=True, blank=True)
+    price_of_materials = models.DecimalField(decimal_places=2)
+    price_of_labour = models.DecimalField(decimal_places=2)
+    quote_price = models.DecimalField(decimal_places=2)
+    issued_by = models.CharField(max_length=255)
+    issued_by_phone_num = models.CharField(max_length=15)
+
+class Address(models.Model):
+    address_line_one = models.CharField(max_length=1024)
+    city = models.CharField(max_length=1024)
+    postcode = models.CharField(max_length=10)
