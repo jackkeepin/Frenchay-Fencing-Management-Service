2dd220c3128719c090206c268a3afb4277c43f1f
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-03-10 18:42:38 +0000

    Functionality to view specific quote. Navbar style updated

--- quote/urls.py
+++ quote/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='quote-home'),
     path('new-quote', views.new_quote, name='new-quote'),
-    path('view-quotes', views.view_quotes, name='view-quotes')
+    path('view-quotes', views.view_quotes, name='view-quotes'),
+    path('view-quote/<str:obj_id>', views.view_quote, name='view-quote')
 ]
 
