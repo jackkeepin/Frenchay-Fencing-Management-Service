8a11dfe2c2b56f584d6e7ab351d4964b82d4976f
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-03-18 18:31:08 +0000

    Changed view-quotes to quote-details and now uses class-based view rather than function-based view

--- quote/urls.py
+++ quote/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from .views import QuoteDetailView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='quote-home'),
     path('new-quote', views.new_quote, name='new-quote'),
     path('view-quotes', views.view_quotes, name='view-quotes'),
-    path('view-quote/<str:obj_id>', views.view_quote, name='view-quote')
+    path('quote-details/<str:obj_id>', QuoteDetailView.as_view(), name='quote-details')
 ]
 
