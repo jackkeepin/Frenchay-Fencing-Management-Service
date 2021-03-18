103d9e27becb9e5b10d6220ce21670829bfd08d4
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-03-18 18:39:37 +0000

    changed view for quote base url

--- quote/urls.py
+++ quote/urls.py
@@ -3,9 +3,9 @@ from .views import QuoteDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='quote-home'),
+    path('', views.view_quotes, name='view-quotes'),
     path('new-quote', views.new_quote, name='new-quote'),
-    path('view-quotes', views.view_quotes, name='view-quotes'),
+    # path('view-quotes', views.view_quotes, name='view-quotes'),
     path('quote-details/<str:obj_id>', QuoteDetailView.as_view(), name='quote-details')
 ]
 
