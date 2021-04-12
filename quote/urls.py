a6078f1d0749d39e29eabd40534966529bdfbc27
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-04-12 15:59:59 +0100

    Created create view for quote and functionality to create quotes

--- quote/urls.py
+++ quote/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import QuoteDetailView
+from .views import QuoteDetailView, QuoteCreateView, QuoteDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.view_quotes, name='view-quotes'),
-    path('new-quote', views.new_quote, name='new-quote'),
+    # path('new-quote', views.new_quote, name='new-quote'),
+    path('new-quote', QuoteCreateView.as_view(), name='new-quote'),
     # path('view-quotes', views.view_quotes, name='view-quotes'),
     path('quote-details/<str:obj_id>', QuoteDetailView.as_view(), name='quote-details')
 ]
