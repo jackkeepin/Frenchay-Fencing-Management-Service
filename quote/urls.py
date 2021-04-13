050d002c2f0404703fa8848efec6cef47f7db42d
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-04-13 12:33:57 +0100

    Implemented update functionality and page for it

--- quote/urls.py
+++ quote/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import QuoteDetailView, QuoteCreateView, QuoteDeleteView
+from .views import QuoteDetailView, QuoteCreateView, QuoteUpdateView, QuoteDeleteView
 from . import views
 
 urlpatterns = [
     path('', views.view_quotes, name='view-quotes'),
     path('new-quote', QuoteCreateView.as_view(), name='new-quote'),
     path('quote-details/<str:obj_id>', QuoteDetailView.as_view(), name='quote-details'),
+    path('quote-update/<str:obj_id>', QuoteUpdateView.as_view(), name='quote-update'),
     path('delete-quote/<obj_id>', QuoteDeleteView.as_view(), name='quote-delete'),
 ]
 
--- quote/views.py
+++ quote/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from quote.models import Quote, get_all_quotes, get_single_quote, QuoteForm
-from django.views.generic import DetailView, CreateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
@@ -55,6 +55,20 @@ class QuoteDetailView(DetailView):
 class QuoteCreateView(CreateView):
     model = Quote
     form_class = QuoteForm
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class QuoteUpdateView(UpdateView):
+    model = Quote
+    form_class = QuoteForm
+    pk_url_kwarg = 'obj_id'
+
+    def get_object(self, queryset=None):
+        quote = get_single_quote(self.kwargs.get('obj_id'))
+        quote.id = quote._id
+        return quote
 
     def form_valid(self, form):
         return super().form_valid(form)
