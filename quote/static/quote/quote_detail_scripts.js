2b70d75cf6fb2ec23f1daf615db0676b36db17e0
Jack Keepin <jack.keepin@students.plymouth.ac.uk>
2021-04-26 17:20:35 +0100

    FIxed bug where button clicks wouldn't do anything

--- quote/static/quote/quote_detail_scripts.js
+++ quote/static/quote/quote_detail_scripts.js
@@ -12,23 +12,3 @@ $( document ).ready(function() {
         $("#materials").append(material + "<br>");
     });
 });
-
-$("#createJobButton").click(function() {
-    $.post("/quote/create-job", {'data': '{{object.id}}'}, function(data) {
-        if (data['error']) {
-            let error_msg = ""
-
-            if ('address' in data['error'] == true) {
-                error_msg = "<h3>A job at this address already exists!</h3>"
-            }
-            else {
-                error_msg = "<h3>You must fill out all fields before you can create a job!</h3>"
-            }
-            $('#createJobError').html(error_msg);
-            $('#createJobError').show();
-        }
-        else {
-            window.location.href = data['success'];
-        }
-    });
-});
